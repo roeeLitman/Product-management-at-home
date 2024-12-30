3f0a275e603db565b25ea0c74ffa050a7bb5873e
roee ltmanovich <roeeli770@gmail.com>
2024-12-30 17:32:20 +0200

    create base schema for listMOdel

--- server/src/models/listModel.ts
+++ server/src/models/listModel.ts
@@ -1,8 +1,41 @@
-import { Types } from "mongoose";
+import { Model, model, Schema, Types } from "mongoose";
+import { ListType } from "../types/enum/ListType";
 
 interface List {
     name: string,
-    listType: string,
+    listType: ListType,
     group: Types.ObjectId,
     prodacts: Types.ObjectId[],
+    createdAt: Date
+    completed?: boolean
 }
+
+const listSchema = new Schema<List>({
+    name: {
+        type: String,
+        required: true
+    },
+    listType: {
+        type: String,
+        enum: Object.values(ListType)
+    },
+    group: {
+        type: Types.ObjectId
+    },
+    prodacts: {
+        type: [{ type: Types.ObjectId, ref: "Product" }]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    completed: {
+        type: Boolean,
+        required: false
+    }
+})
+
+const ListModel: Model<List> = model<List>("List", listSchema);
+
+export default ListModel
+
