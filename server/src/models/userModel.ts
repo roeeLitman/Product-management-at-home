9e2a311f938b843f64f39e42a81f00bc6cd4757c
roee ltmanovich <roeeli770@gmail.com>
2024-12-30 16:32:09 +0200

    create base project

---
+++ server/src/models/userModel.ts
@@ -0,0 +1,16 @@
+import { Schema } from "mongoose"
+
+const userSchma = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
