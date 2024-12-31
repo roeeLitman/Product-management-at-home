2db71e53fe0435c9fc41a831fe9c73b817cd61ee
roee ltmanovich <roeeli770@gmail.com>
2024-12-31 13:57:53 +0200

    add corse and dotenv pacages

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
 
