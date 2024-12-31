b5c4082249676524b0691875315e477acb7c5919
roee ltmanovich <roeeli770@gmail.com>
2024-12-31 13:40:01 +0200

    create prodact model

---
+++ server/src/models/prodactModel.ts
@@ -0,0 +1,44 @@
+import { Model, model, Schema, Types } from "mongoose";
+
+export interface Product {
+    name: string;
+    price: number;
+    isInStock: boolean;
+    amount: number;
+    list: Types.ObjectId;
+    isWeight: boolean;
+}
+
+const productSchema = new Schema<Product>({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    isInStock: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    amount: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    list: {
+        type: Types.ObjectId,
+        required: true,
+    },
+    isWeight: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+});
+
+const ProductModel: Model<Product> = model<Product>("Product", productSchema);
+
+export default ProductModel
