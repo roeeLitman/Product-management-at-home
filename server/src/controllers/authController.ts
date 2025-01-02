bd679b91da031c6c968b7485fe0a12446d4e3695
roee ltmanovich <roeeli770@gmail.com>
2025-01-02 23:32:27 +0200

    change name of interface

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models/userModel";
 import { ResData } from "../types/interface/resData";
-import { RegisterDTO } from "../types/DTO/registerDTO";
+import { UserDTO } from "../types/DTO/userDTO";
 import { createUserService } from "../services/aothService";
 
 export const createUser = async (
-    req: Request<any, any, any, RegisterDTO>,
+    req: Request<any, any, any, UserDTO>,
     res: Response,
     next: NextFunction
 ): Promise<void> => {
