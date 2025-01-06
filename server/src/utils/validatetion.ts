e5fc18650efbd1a712bdc887a22d70b0a0978609
roee ltmanovich <roeeli770@gmail.com>
2025-01-06 15:33:40 +0200

    cretae function for crete token

--- server/src/utils/validatetion.ts
+++ server/src/utils/validatetion.ts
@@ -1,6 +1,10 @@
 import AppError from "../types/class/appErore";
 import bcrypt from "bcrypt";
+import { Payload } from "../types/interface/payloade";
+import jwt from "jsonwebtoken";
 
+
+// compare password
 export async function isMatch(
     password: string,
     hashPassword: string
@@ -9,9 +13,17 @@ export async function isMatch(
         // compare password
         const isPasswordMatch = await bcrypt.compare(password, hashPassword);
         // check if password is correct or not
-        if(!isPasswordMatch) throw new AppError("password or name is incorrect", 401)
+        if (!isPasswordMatch)
+            throw new AppError("password or name is incorrect", 401);
         return isPasswordMatch;
     } catch (err: any) {
         throw err;
     }
+}
+
+// create token
+export function createToken(payload: Payload): string {
+    return jwt.sign(payload, process.env.JWT_SECRET as string, {
+        expiresIn: "1h",
+    });
 }
